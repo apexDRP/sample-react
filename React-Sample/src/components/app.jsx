cb9fccb5371cde42884d88cf5e03bca439a71a0f
apexDRP <48053145+apexDRP@users.noreply.github.com>
2019-05-27 21:42:38 +0530

    Add files via upload

---
+++ React-Sample/src/components/app.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Header from './header';
+import Form from './form';
+
+class App extends Component{
+    render() {
+        return<div>
+            <Header/>
+            <Form/>
+        </div>
+    }
+}
+
+export default App;
---
+++ React-Sample/src/components/form.jsx
@@ -0,0 +1,86 @@
+import React, {Component} from 'react';
+import '../web_resources/css/style.css';
+
+//inline style for dialogEPay component
+const formBox = {
+        fontSize: '1vw',
+        fontFamily: 'poppins',
+        float: 'left',
+        width: '35vw',
+        height: 'auto',
+        padding: '1vw',
+        margin: '2vw',
+        boxSizing: 'border-box',
+        border: '0.18vw solid #ff0000',
+        borderRadius: '1.3vw',
+    }
+
+class Form extends Component {
+    constructor(props) {
+        super(props);
+
+        //initializes the class properties
+        this.state = {
+            name: "",
+            birthDate: "",
+            submitData: [{
+                submitName: "",
+                submitBirthDate:""
+            }],
+        }
+
+        //passing class context to each method.
+        this.changeHandler = this.changeHandler.bind(this);
+        this.submitHandler = this.submitHandler.bind(this);
+    }
+
+    /*
+    this triggers when user type inputs in inputs fields
+    this store input fields values to class property values
+    */
+    changeHandler(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    /*
+    this triggers when user click the submit button
+    */
+    submitHandler(event) {
+        event.preventDefault(); //prevent refresh of input values
+        alert(`Name: ${this.state.name}\nBirthday: ${this.state.birthDate}`);
+    }
+
+    render() {
+        //retrieve today's date
+        let today = new Date().toISOString().substr(0, 10);
+
+        return <div style={formBox}>
+            <form onSubmit={this.submitHandler}>
+                <table>
+                    <tbody>
+                    <tr>
+                        <td>Name</td>
+                        <td><input type="text" name="name" value={this.state.name}
+                                   onChange={this.changeHandler} className='textBox' autoComplete='off' required/></td>
+                    </tr>
+                    <tr>
+                        <td>Birthday</td>
+                        <td><input type="date" max={today} name="birthDate" value={this.state.birthDate}
+                                   onChange={this.changeHandler}
+                                   className='textBox' required/></td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <button type="submit" className='button'>Submit</button>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </form>
+        </div>
+    }
+}
+
+export default Form;
